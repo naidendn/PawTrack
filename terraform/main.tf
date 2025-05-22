a831259a24c1093bcc33e585bfc3997d2b91ef00
Nayden Nikolov <naidenn@gmail.com>
2025-05-22 21:56:58 +0300

    Updating terraform files

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,1 +1,27 @@
 # comment
+provider "aws" {
+  region = "eu-central-1"
+}
+
+resource "aws_dynamodb_table" "sensor_data" {
+  name = "sensor_data"
+  billing_mode = "PAY_PER_REQUEST"
+
+  hash_key = "room_id"
+  range_key = "timestamp"
+
+  attribute {
+    name = "room_id"
+    type = "S"
+  }
+
+  attribute {
+    name = "timestamp"
+    type = "S"
+  }
+
+  tags = {
+    Name = "sensor_data"
+    Environment = "dev"
+  }
+}
